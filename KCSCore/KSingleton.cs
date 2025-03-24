668e6cd1c559abbd289c3f5035540d3d4d535554
kos-y <kosyos.address@gmail.com>
2025-03-24 19:37:36 +0900

    汎用Singletonクラス追加

---
+++ KCSCore/KSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSCore
+{
+    /// <summary>
+    /// Singletonクラス
+    /// </summary>
+    /// <typeparam name="T">Singletonにするクラス</typeparam>
+    public static class KSingleton<T> where T : new()
+    {
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        private static readonly T s_instance = new();
+
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static T Instance => s_instance;
+    }
+}
