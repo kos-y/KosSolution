075b019942f13ea0efd46a2f2dc980beddbc2d80
kos-y <kosyos.address@gmail.com>
2025-03-25 20:52:07 +0900

    PokeAPIのクラス定義

---
+++ KCSPokeAPI/KMachine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 技マシン
+/// </summary>
+public class KMachine
+{
+    /// <summary>
+    /// 技マシンID
+    /// </summary>
+    public int ID { get; set; }
+
+    /// <summary>
+    /// アイテム
+    /// </summary>
+    public KNamedAPIResource Item { get; set; } = new();
+
+    /// <summary>
+    /// 技
+    /// </summary>
+    public KNamedAPIResource Move { get; set; } = new();
+
+    /// <summary>
+    /// バージョングループ
+    /// </summary>
+    public KNamedAPIResource VersionGroup { get; set; } = new();
+}
