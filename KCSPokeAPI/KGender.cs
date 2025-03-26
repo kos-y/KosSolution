08868a16486020b479f509bbdd77aa040a1fabb6
kos-y <kosyos.address@gmail.com>
2025-03-26 20:30:49 +0900

    PokeAPIのクラス定義（Genders）

---
+++ KCSPokeAPI/KGender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+public class KGender
+{
+    /// <summary>
+    /// 性別ID
+    /// </summary>
+    public int ID { get; set; }
+
+    /// <summary>
+    /// 性別名
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// ポケモン種族リスト
+    /// </summary>
+    public List<KPokemonSpeciesGender> PokemonSpeciesDetails { get; set; } = [];
+
+    /// <summary>
+    /// 性別が要求される進化
+    /// </summary>
+    public List<KNamedAPIResource> RequiredForEvolution { get; set; } = [];
+}
---
+++ KCSPokeAPI/KPokemonSpeciesGender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// ポケモン種族の性別
+/// </summary>
+public class KPokemonSpeciesGender
+{
+    /// <summary>
+    /// 確率
+    /// </summary>
+    public int Rate { get; set; }
+
+    /// <summary>
+    /// ポケモン種族
+    /// </summary>
+    public KNamedAPIResource PokemonSpecies { get; set; } = new();
+}
