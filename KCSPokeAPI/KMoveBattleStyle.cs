f77729d0473d766bc03d114f5f2a55e6aabf5ac5
kos-y <kosyos.address@gmail.com>
2025-03-26 19:58:26 +0900

    PokeAPIのクラス定義

---
+++ KCSPokeAPI/KMoveBattleStyle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 技の戦闘スタイル
+/// </summary>
+public class KMoveBattleStyle
+{
+    /// <summary>
+    /// 技の戦闘スタイルID
+    /// </summary>
+    public int ID { get; set; }
+
+    /// <summary>
+    /// 技の戦闘スタイル名
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 技の戦闘スタイル名リスト
+    /// </summary>
+    public List<KName> Names { get; set; } = [];
+}
---
+++ KCSPokeAPI/KMoveCategory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 技のカテゴリ
+/// </summary>
+public class KMoveCategory
+{
+    /// <summary>
+    /// 技のカテゴリID
+    /// </summary>
+    public int ID { get; set; }
+
+    /// <summary>
+    /// 技のカテゴリ名
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 技リスト
+    /// </summary>
+    public List<KNamedAPIResource> Moves { get; set; } = [];
+
+    /// <summary>
+    /// 説明リスト
+    /// </summary>
+    public List<KDescription> Descriptions { get; set; } = [];
+}
