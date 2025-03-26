b6a3b9d775f5cba80e112372aec969381b1ff75e
kos-y <kosyos.address@gmail.com>
2025-03-26 20:59:45 +0900

    PokeAPIのクラス定義（Pokeathlon Stats）

---
+++ KCSPokeAPI/KNaturePokeathlonStatAffect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 性格が影響を与えるポケスロンのステータス
+/// </summary>
+public class KNaturePokeathlonStatAffect
+{
+    /// <summary>
+    /// 最大の変更料
+    /// </summary>
+    public int MaxChange { get; set; }
+
+    /// <summary>
+    /// 性格
+    /// </summary>
+    public KNamedAPIResource Nature { get; set; } = new();
+}
---
+++ KCSPokeAPI/KNaturePokeathlonStatAffectSets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 性格が影響を与えるポケスロンのステータスセット
+/// </summary>
+public class KNaturePokeathlonStatAffectSets
+{
+    /// <summary>
+    /// 上昇するステータスリスト
+    /// </summary>
+    public List<KNaturePokeathlonStatAffect> Increase { get; set; } = [];
+
+    /// <summary>
+    /// 下降するステータスリスト
+    /// </summary>
+    public List<KNaturePokeathlonStatAffect> Decrease { get; set; } = [];
+}
