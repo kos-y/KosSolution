abdb8ba42fb881f2e3b297868a3c36933bda58b9
kos-y <kosyos.address@gmail.com>
2025-03-26 22:15:52 +0900

    PokeAPIのクラス定義（Pokemon Shapes）

---
+++ KCSPokeAPI/KAwesomeName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 愛称
+/// </summary>
+public class KAwesomeName
+{
+    /// <summary>
+    /// 愛称
+    /// </summary>
+    public string AwesomeName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 言語
+    /// </summary>
+    public KNamedAPIResource Language { get; set; } = new();
+}
