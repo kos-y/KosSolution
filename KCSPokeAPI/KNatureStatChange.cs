e41074374153fa7ca67811aa805e618625c4aeb1
kos-y <kosyos.address@gmail.com>
2025-03-26 20:53:03 +0900

    PokeAPIのクラス定義（Natures）

---
+++ KCSPokeAPI/KNatureStatChange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 性格がポケスロンに影響を与えるステーたる
+/// </summary>
+public class KNatureStatChange
+{
+    /// <summary>
+    /// 最大変更量
+    /// </summary>
+    public int MaxChange { get; set; }
+
+    /// <summary>
+    /// ポケスロンのステータス
+    /// </summary>
+    public KNamedAPIResource PokeathlonStat { get; set; } = new();
+}
