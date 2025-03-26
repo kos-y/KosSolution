3dfcbc678a9c54558c2f3a524d3ec4cc7ba3f78b
kos-y <kosyos.address@gmail.com>
2025-03-26 21:57:41 +0900

    PokeAPIのクラス定義（Pokemon Location Areas）

---
+++ KCSPokeAPI/KLocationAreaEncounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// エンカウントする場所エリア
+/// </summary>
+public class KLocationAreaEncounter
+{
+    /// <summary>
+    /// 場所エリア
+    /// </summary>
+    public KNamedAPIResource LocationArea { get; set; } = new();
+
+    /// <summary>
+    /// バージョンごとの詳細
+    /// </summary>
+    public List<KVersionEncounterDetail> VersionDetails { get; set; } = [];
+}
