bee10cfbdedc316d347319c3961419876b7d8ecf
kos-y <kosyos.address@gmail.com>
2025-03-27 20:00:35 +0900

    PokeAPIのクラス定義（Stats）

---
+++ KCSPokeAPI/KMoveStatAffectSets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 技が影響を与えるステータス
+/// </summary>
+public class KMoveStatAffectSets
+{
+    /// <summary>
+    /// いい影響を与えるステータス
+    /// </summary>
+    public List<KMoveStatAffect> Increase { get; set; } = [];
+
+    /// <summary>
+    /// 悪い影響を与えるステータス
+    /// </summary>
+    public List<KMoveStatAffect> Decrease { get; set; } = [];
+}
