a4b35a93f33e59e71fbbcb65f4ef7c09ce16e643
kos-y <kosyos.address@gmail.com>
2025-03-27 20:12:12 +0900

    PokeAPIのクラス定義（Types）

---
+++ KCSPokeAPI/KTypePokemon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// ポケモンタイプ
+/// </summary>
+public class KTypePokemon
+{
+    /// <summary>
+    /// スロット
+    /// </summary>
+    public int Slot { get; set; }
+
+    /// <summary>
+    /// ポケモン
+    /// </summary>
+    public KNamedAPIResource Pokemon { get; set; } = new();
+}
---
+++ KCSPokeAPI/KTypeRelationsPast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCSPokeAPI;
+
+/// <summary>
+/// 過去のタイプ相性
+/// </summary>
+public class KTypeRelationsPast
+{
+    /// <summary>
+    /// 世代
+    /// </summary>
+    public KNamedAPIResource Generation { get; set; } = new();
+
+    /// <summary>
+    /// タイプ相性
+    /// </summary>
+    public KTypeRelations DamageRelations { get; set; } = new();
+}
